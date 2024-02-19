9dfa4a7c49f69f1d3325b0d39a1295227e181831
Shakti Sarada Prasad <83201466+shakti-sarada@users.noreply.github.com>
2024-02-19 23:57:26 +0530

    model added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from AlzheimerDiseaseClassification import logger
 from AlzheimerDiseaseClassification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from AlzheimerDiseaseClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from AlzheimerDiseaseClassification.pipeline.stage_03_model_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -20,6 +22,19 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
